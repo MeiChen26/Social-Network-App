e8b07e828b2ebdb2eebfcc80764e18fb42e26ab3
meitong <meitong@meitongdeMacBook-Pro.local>
2019-02-16 10:28:49 +0800

    main register

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { TopBar } from './TopBar';
+import { Main } from './Main';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <TopBar/>
+        <Main/>
       </div>
     );
   }
